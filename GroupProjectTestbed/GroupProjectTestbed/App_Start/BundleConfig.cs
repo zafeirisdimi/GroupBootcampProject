5f6baf82d6ba336da24b32426ff662072809aa74
Dimitris Zafeiris <kremou115@gmail.com>
2022-07-14 23:01:24 +0300

    last changes

--- GroupProjectTestbed/GroupProjectTestbed/App_Start/BundleConfig.cs
+++ GroupProjectTestbed/GroupProjectTestbed/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace GroupProjectTestbed
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/template_styles.css"));
         }
     }
 }
